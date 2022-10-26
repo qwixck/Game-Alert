fa6de702fe5adbf391a35445d42fc2a857d70abb
qwixck <erikmozeika@gmail.com>
2022-10-26 13:52:29 +0300

    added error handlers

--- cogs/events.py
+++ cogs/events.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 
 class Events(commands.Cog):
@@ -7,6 +8,20 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print(f"{self.bot.user} is online!")
+
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
+        print(error)
+        owner = await self.bot.fetch_user(self.bot.owner_id)
+        await owner.send(f"Error!\n```\n{error}\n```")
+        await ctx.respond("There's something wrong with me! Owner was already notified. Please wait for fix.")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: discord.ApplicationContext, error: discord.DiscordException):
+        print(error)
+        owner = await self.bot.fetch_user(self.bot.owner_id)
+        await owner.send(f"Error!\n```\n{error}\n```")
+        await ctx.send("There's something wrong with me! Owner was already notified. Please wait for fix.")
 
 def setup(bot: commands.Bot):
     bot.add_cog(Events(bot))
