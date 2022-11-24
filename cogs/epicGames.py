1345da883905cdfce6c7e0933db5348ee7704666
qwixck <erikmozeika@gmail.com>
2022-11-24 18:29:14 +0200

    changed embed's image to wide

--- cogs/epicGames.py
+++ cogs/epicGames.py
@@ -25,7 +25,7 @@ class EpicGames(commands.Cog):
         for i in epicGames["data"]["Catalog"]["searchStore"]["elements"]:
             if i["price"]["totalPrice"]["discountPrice"] == 0 and len(i["price"]["lineOffers"][0]["appliedRules"]) != 0 and datetime.datetime.fromisoformat(str(i["price"]["lineOffers"][0]["appliedRules"][0]["endDate"]).replace("Z", "")) >= datetime.datetime.now() and i["title"] not in games:
                 embed = discord.Embed(title=i["title"], description=i["description"], color=discord.Color.blurple())
-                embed.set_image(url=i["keyImages"][1]["url"])
+                embed.set_image(url=i["keyImages"][0]["url"])
                 embed.set_footer(text="Epic Games")
                 list.append(embed)
                 games.append(i["title"])
